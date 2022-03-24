614fe536a93e6977dfc23619a33298813dcec0cc
aasalma <salma.aziz-alaoui@insa-lyon.fr>
2022-03-24 15:40:01 +0100

    slider rayon de recherche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,6 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgxSliderModule,
     BrowserAnimationsModule
   ],
   providers: [],

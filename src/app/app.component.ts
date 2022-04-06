824d2081248facb61952d126e4997bf1e1dce156
Julie Breuil <jbpub@posteo.net>
2022-04-06 20:05:52 +0200

    maching devient matching changement infos des matchs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Maching} from "./models/maching.models";
+import {Matching} from "./models/matching.models";
 
 @Component({
   selector: 'app-root',
@@ -7,66 +7,34 @@ import {Maching} from "./models/maching.models";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'PWEB-Front';
-  matchs!: Maching[];
+  //title = 'PWEB-Front';
+  matchs!: Matching[];
   ngOnInit() {
     this.matchs = [
       {
-        prenom: 'Julie',
-        nom: 'Breuil',
+        prenom: 'Soiree',
+        nom: 'DUB',
         imageUrl: 'https://www.karnaval.fr/wp-content/uploads/2022/02/SoireeDUB_MLutz-9.jpg',
         profession: 'choooooomage',
         position: 'quelque part dans Villeurbanne',
         description: 'J apprécie particulierement les crepes au caramel',
       },
       {
-        prenom: 'Alex',
-        nom: 'Duroy',
+        prenom: 'Marmite',
+        nom: 'La foliiiie',
         imageUrl: 'https://www.karnaval.fr/wp-content/uploads/2022/02/Karna-Marmite-WM-AD-28.jpg',
         profession: 'jongleur',
         position: 'Dieulefit',
         description: 'Il y a eu un kouak organisationnel de type majeur',
       },
       {
-        prenom: 'Sapristi',
-        nom: 'Nolala',
+        prenom: 'RLK',
+        nom: 'chapiteaui',
         imageUrl: 'https://www.karnaval.fr/wp-content/uploads/2022/02/Karna-BalFolk-WM-AD-03.jpg',
         profession: 'Monteur de chapchapchap',
         position: 'Sur la pelouse des Humanités',
         description: 'Ceci dit, je ne suis pas sûr que cela soit important',
       },
-
     ]
   }
 }
-
-/*export class AppComponent implements OnInit{
-  faceSnaps!: FaceSnap[];
-  ngOnInit() {
-    this.faceSnaps = [
-      {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis tout petit !',
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        location: 'Paris'
-      },
-      {
-        title: 'Karnakorale',
-        description: 'Je vous le dis',
-        imageUrl: 'https://www.karnaval.fr/wp-content/uploads/2022/02/DSC_8683-scaled.jpg',
-        createdDate: new Date(),
-        snaps: 6,
-        location: 'chapiteau du Karna'
-      },
-      {
-        title: 'Karnadahl',
-        description: 'Miam',
-        imageUrl: 'https://www.karnaval.fr/wp-content/uploads/2022/03/karnadahl.jpg',
-        createdDate: new Date(),
-        snaps: 160
-      },
-    ]
-  }
-}*/

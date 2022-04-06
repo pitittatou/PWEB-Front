873f403a092f6cd62ffaa83457f88e4a1be2f2a7
Julie Breuil <jbpub@posteo.net>
2022-04-06 20:05:11 +0200

    import de MatchingComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatchingComponent } from "./matching/matching.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchingComponent
   ],
   imports: [
     BrowserModule,

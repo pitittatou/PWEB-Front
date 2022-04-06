59ebcd808d5cc6882e9e514406ebe6d970d53f05
Julie Breuil <jbpub@posteo.net>
2022-04-06 18:40:35 +0200

    rename de maching.infos.models.ts creation des 3 fichiers maching.component avec pas grand chose dedans

---
+++ src/app/models/maching.models.ts
@@ -0,0 +1,10 @@
+export class Maching {
+  prenom!: string;
+  nom!: string;
+  imageUrl!: string;
+  profession!: string;
+  position!: string;
+  description!: string;
+  //button pour swipe oui et non
+  //button pour faire passer de droite à gauche les differentes photos
+}

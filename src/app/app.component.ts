5c48ca040512bb7dfead32d5614fdc5f35ff7000
Ines Benmoussa <benmoussaines08@gmail.com>
2022-04-08 11:09:11 +0200

    navbar 3 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,11 @@
 import { Component } from '@angular/core';
 
-
-export interface Section {
-  name: string;
-  updated: Date;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 
-
-
 export class AppComponent {
- 
   title = 'PWEB-Front';
-
-  
-
 }

f7de890ed96c785be27da49472799f23f511e33b
Julie Breuil <jbpub@posteo.net>
2022-04-08 11:18:43 +0200

    le composant n'est plus dans app.component.ts mais dans matching.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {Matching} from "./models/matching.models";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   //title = 'PWEB-Front';
-  matchs!: Matching[];
-  ngOnInit() {
-    this.matchs = [
-      {
-        prenom: 'Jonas',
-        age: 36,
-        imageUrl: 'https://www.karnaval.fr/wp-content/uploads/2022/02/SoireeDUB_MLutz-9.jpg',
-        profession: 'pâtisser',
-        position: 'Villeurbanne',
-        description: 'J apprécie particulierement les crepes au caramel',
-      },
-      /*{
-        prenom: 'Titus',
-        age: 20,
-        imageUrl: 'https://www.karnaval.fr/wp-content/uploads/2022/02/Karna-Marmite-WM-AD-28.jpg',
-        profession: 'jongleur',
-        position: 'Dieulefit',
-        description: 'Il y a eu un kouak organisationnel de type majeur',
-      },*/
-      /*{
-        prenom: 'Polinichel',
-        age: 18,
-        imageUrl: 'https://www.karnaval.fr/wp-content/uploads/2022/02/Karna-BalFolk-WM-AD-03.jpg',
-        profession: 'Monteur de chapchapchap',
-        position: 'Sur la pelouse des Humanités',
-        description: 'Ceci dit, je ne suis pas sûr que cela soit important',
-      },*/
-    ]
-  }
 }
--- src/app/matching/matching.component.ts
+++ src/app/matching/matching.component.ts
@@ -1,5 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
-import {Matching} from "../models/matching.models";
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-matching',
@@ -7,7 +6,17 @@ import {Matching} from "../models/matching.models";
   styleUrls: ['./matching.component.scss']
 })
 export class MatchingComponent implements OnInit{
-  @Input() matching!: Matching;
+  prenom!: string;
+  age!: number;
+  imageUrl!: string;
+  profession!: string;
+  position!: string;
+  description!: string;
   ngOnInit() {
+    this.prenom = 'Jonas';
+    this.age= 36;
+    this.imageUrl='https://www.karnaval.fr/wp-content/uploads/2022/02/SoireeDUB_MLutz-9.jpg';
+    this.profession='patissier';
+    this.description='J apprécie particulierement les crepes au caramel';
   }
 }

8be63aad804b480545b895181d07ac9af91807bc
aasalma <salma.aziz-alaoui@insa-lyon.fr>
2022-04-24 03:39:58 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-//import { Options } from '@angular-slider/ngx-slider';
+import { Options } from '@angular-slider/ngx-slider';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  value: number = 100;
+  options: Options = {
+    floor: 0,
+    ceil: 200
+  };
+  minValue: number = 50;
+  maxValue: number = 200;
+  Options: Options = {
+    floor: 18,
+    ceil: 100,
+  };
 }

a90e208e0f2a778ed3a81ce78013ca4eeb0e7064
Hugo THOMAS <thomas.hugo18@sfr.fr>
2022-05-06 17:28:30 +0200

    implement routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ApercuComponentComponent } from './apercu-component/apercu-component.component';
 import { ProfilComponent } from './profil/profil.component';
+import {MatchingComponent} from "./matching/matching.component";
+import {LoginComponent} from "./login/login.component";
+import {PhotoChooserComponent} from "./photo-chooser/photo-chooser.component";
 
 const routes: Routes = [
-  { path: 'pageApercu', component: ApercuComponentComponent },
-  { path: 'Profil', component: ProfilComponent },
+  { path: '', component: MatchingComponent },
+  { path: 'connexion', component: LoginComponent },
+  { path: 'profil', component: ProfilComponent },
+  { path: 'photos', component: PhotoChooserComponent }, // temporaire
 ];
 
 @NgModule({

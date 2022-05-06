5337e896d43cee2074082d5276bc4ae2a33c5b27
gb4400 <62654326+gb4400@users.noreply.github.com>
2022-05-06 17:35:08 +0200

    swiper v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Options } from '@angular-slider/ngx-slider';
+import { SwiperOptions } from 'swiper';
+    
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,38 @@ import { Options } from '@angular-slider/ngx-slider';
 
 export class AppComponent {
   title = 'PWEB-Front';
+
+  Images: Array<object> = [
+    {
+      src: 'https://loremflickr.com/g/600/400/paris',
+      alt: 'Image 1',
+    }, {
+      src: 'https://loremflickr.com/600/400/brazil,rio',
+      alt: 'Image 2'
+    }, {
+      src: 'https://loremflickr.com/600/400/paris,girl/all',
+      alt: 'Image 3'
+    }, {
+      src: 'https://loremflickr.com/600/400/brazil,rio',
+      alt: 'Image 4'
+    }, {
+      src: 'https://loremflickr.com/600/400/paris,girl/all',
+      alt: 'Image 5'
+    }, {
+      src: 'https://loremflickr.com/600/400/brazil,rio',
+      alt: 'Image 6'
+    }    
+  ]
+  config: SwiperOptions = {
+    pagination: { 
+      el: '.swiper-pagination', 
+      clickable: true 
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    spaceBetween: 30
+  };  
 
 }

af1aed02ed425964a4329dd65dffa2ad5ac05c7a
Floriane <floriane.caillieret@gmail.com>
2022-05-09 20:57:38 +0200

    Revert "logo à revoir"
    This reverts commit b4d79e81da12dd625be6c239e5adeec58bee510f.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { Options } from '@angular-slider/ngx-slider';
 import { FormControl, FormGroup } from '@angular/forms';
-import { GoogleLoginProvider, SocialAuthService } from 'angularx-social-login';
-import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +8,6 @@ import {Router} from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  constructor(
-      private socialAuthService: SocialAuthService) {
-  }
-
-  loginWithGoogle(): void {
-    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)
-  }
-
   hide = true;
   isActive = true;
   sliderControl: FormControl = new FormControl(100);

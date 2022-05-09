b4d79e81da12dd625be6c239e5adeec58bee510f
Floriane <floriane.caillieret@gmail.com>
2022-05-09 20:40:23 +0200

    logo à revoir

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Options } from '@angular-slider/ngx-slider';
 import { FormControl, FormGroup } from '@angular/forms';
+import { GoogleLoginProvider, SocialAuthService } from 'angularx-social-login';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +10,14 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  constructor(
+      private socialAuthService: SocialAuthService) {
+  }
+
+  loginWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)
+  }
+
   hide = true;
   isActive = true;
   sliderControl: FormControl = new FormControl(100);

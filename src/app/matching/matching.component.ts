9b7217295c0a7924c12d37490478ead32f623511
Hugo THOMAS <thomas.hugo18@sfr.fr>
2022-05-13 16:36:52 +0200

    Merge Julie into dev

--- src/app/matching/matching.component.ts
+++ src/app/matching/matching.component.ts
@@ -1,22 +1,70 @@
 import {Component, OnInit} from '@angular/core';
+import {user} from "../models/model_user";
+
 
 @Component({
   selector: 'app-matching',
   templateUrl: './matching.component.html',
   styleUrls: ['./matching.component.scss']
 })
+
 export class MatchingComponent implements OnInit{
-  prenom!: string;
-  age!: number;
-  imageUrl!: string;
-  profession!: string;
-  position!: string;
-  description!: string;
+  monUser!: user[];
+  i!:number;
   ngOnInit() {
-    this.prenom = 'Jonas';
-    this.age= 36;
-    this.imageUrl='https://www.karnaval.fr/wp-content/uploads/2022/02/SoireeDUB_MLutz-9.jpg';
-    this.profession='patissier';
-    this.description='J apprécie particulierement les crepes au caramel';
+    this.monUser=[
+      {
+        prenom : 'Jonas',
+        age: 36,
+        imageUrl:'https://www.karnaval.fr/wp-content/uploads/2022/02/SoireeDUB_MLutz-9.jpg',
+        imageUrlCenter:'https://www.karnaval.fr/wp-content/uploads/2022/02/SoireeDUB_MLutz-9.jpg',
+        imageUrlLeft:'https://www.karnaval.fr/wp-content/uploads/2022/02/Karna-Dub-WM-AD-05.jpg',
+        imageUrlRight:'https://www.karnaval.fr/wp-content/uploads/2022/02/Karna-Dub-WM-AD-09.jpg',
+        profession:'patissier',
+        position:'Lyon',
+        description:'J apprécie particulierement les crepes au caramel'
+      },
+      {
+        prenom : 'Elektruka',
+        age: 32,
+        imageUrl:'https://www.karnaval.fr/wp-content/uploads/2022/04/Karna_Bal_Folk_PLeCorre-69.jpg',
+        imageUrlCenter:'https://www.karnaval.fr/wp-content/uploads/2022/04/Karna_Bal_Folk_PLeCorre-69.jpg',
+        imageUrlLeft:'https://www.karnaval.fr/wp-content/uploads/2022/04/Karna_Bal_Folk_PLeCorre-64.jpg',
+        imageUrlRight:'https://www.karnaval.fr/wp-content/uploads/2022/04/Karna_Bal_Folk_PLeCorre-62.jpg',
+        profession:'accordeoniste',
+        position:'Pelouse des Humas',
+        description:'J accompagne le public à la sortie'
+      }
+    ];
+    this.i=0;
+  }
+
+  onClickLeft(){
+    console.log("J'ai cliqué sur le bouton de gauche");
+    if (this.monUser[this.i].imageUrl == this.monUser[this.i].imageUrlCenter){
+      this.monUser[this.i].imageUrl=this.monUser[this.i].imageUrlLeft;
+    }else if (this.monUser[this.i].imageUrl == this.monUser[this.i].imageUrlRight){
+      this.monUser[this.i].imageUrl=this.monUser[this.i].imageUrlCenter;
+    }else {
+      this.monUser[this.i].imageUrl = this.monUser[this.i].imageUrlRight;
+    }
+  }
+
+  onClickRight(){
+    console.log("J'ai cliqué sur le bouton de droite");
+    if (this.monUser[this.i].imageUrl == this.monUser[this.i].imageUrlCenter){
+      this.monUser[this.i].imageUrl=this.monUser[this.i].imageUrlRight;
+    }else if (this.monUser[this.i].imageUrl == this.monUser[this.i].imageUrlLeft){
+      this.monUser[this.i].imageUrl=this.monUser[this.i].imageUrlCenter;
+    }else{
+      this.monUser[this.i].imageUrl=this.monUser[this.i].imageUrlLeft;
+    }
+  }
+
+  onClickNext(){
+    console.log("J'ai cliqué sur le bouton pour voir le prochain profil");
+    if (this.monUser.length-1 <= this.i+1){
+      this.i=this.i+1;
+    }
   }
 }
---
+++ src/app/models/model_user.ts
@@ -0,0 +1,11 @@
+export class user {
+    prenom!: string;
+    age!: number;
+    imageUrl!: string;
+    imageUrlCenter!: string;
+    imageUrlLeft!: string;
+    imageUrlRight!: string;
+    profession!: string;
+    position!: string;
+    description!: string;
+}

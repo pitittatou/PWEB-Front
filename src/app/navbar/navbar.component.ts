e25f6247ee30d20e17e7dc738d8ac30b0791db3e
Hugo THOMAS <thomas.hugo18@sfr.fr>
2022-05-16 22:41:55 +0200

    bug fix

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
-import {AuthenticationServie} from "../services/authentication.service";
+import {AuthenticationService} from "../services/authentication.service";
 
 export interface Section {
   name: string;

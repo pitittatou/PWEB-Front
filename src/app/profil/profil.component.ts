15a9c9bedee4e4724af02bf7e677e9aedb30add3
aasalma <salma.aziz-alaoui@insa-lyon.fr>
2022-05-17 14:48:38 +0200

    up

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,26 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Options } from '@angular-slider/ngx-slider';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ProfilService } from '../services/Profil.service';
 import { NgForm } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+interface attirance {
+  name: string;
+  selected: boolean;
+}
 
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
   styleUrls: ['./profil.component.scss']
 })
-export class ProfilComponent{
+export class ProfilComponent implements OnInit {
   Data!:any
+  gender!: String[]
+  genderControl!: FormControl 
+  attirances!: attirance[]
+  constructor(public ProfilService: ProfilService) {}
 
-  constructor(public ProfilService: ProfilService) { }
+  ngOnInit(): void {
+    this.gender =["Homme","Femme","Autre"]
+    this.genderControl= new FormControl('', [Validators.required])
+    this.attirances = [
+        <attirance> {name: 'Homme', selected: false},
+        <attirance> {name: 'Femme', selected: false},
+        <attirance> {name: 'Autre', selected: false}
+      ]
+  }
+
+  onChange(id: number, isChecked: boolean) {
+   this.attirances[id].selected = isChecked
+
+  }
 
   Enregistrer(descriptionForm:NgForm){
     //document.getElementById('add-Profil-form')?.click();
     console.log("début fonction enregistrer")
-
+    let drive:string[]=[]
+    for (let i=0; i<this.attirances.length-1;i++){
+      if(this.attirances[i].selected == true){
+        drive.push(this.attirances[i].name)
+      }
+    }
     const postbody={
-      description:descriptionForm.value.Description, 
-      homme:descriptionForm.value.Homme
+      Description:descriptionForm.value.Description, 
+      Genre: this.genderControl.value,
+      Attirance: drive
+      
+      //homme:descriptionForm.value.Homme
     }; 
     this.ProfilService.getData(postbody).subscribe(data => {this.Data=data});
      console.log(postbody);

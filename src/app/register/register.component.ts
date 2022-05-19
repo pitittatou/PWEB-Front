e33988907482dd4f32bd7f5f45b1802f29cb6490
Floriane <floriane.caillieret@gmail.com>
2022-05-19 15:56:34 +0200

    form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Validators } from '@angular/forms';
 import { ProfilService } from '../services/Profil.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
 	selector: 'app-register',
@@ -12,6 +13,11 @@ import { ProfilService } from '../services/Profil.service';
 })
 export class RegisterComponent {
 	registerPage!: boolean;
+	attirances = [
+		<attirance> {name: "Homme", selected: false},
+		<attirance> {name: "Femme", selected: false},
+		<attirance> {name: "Autre", selected: false}
+	]
 
 	constructor( public ProfilService: ProfilService, private router: Router) {}
 
@@ -25,23 +31,16 @@ export class RegisterComponent {
 		return this.registerPage;
 	}
 	
-
-	sliderControl: FormControl = new FormControl(100);
+	value: number = 100;
 	options: Options = {
-		floor: 0,
-		ceil: 50,
-		getPointerColor: (value: number): string => {
-			return '#34c6bb';
-		}
+		floor: 1,
+		ceil: 200
 	};
-	sliderForm: FormGroup = new FormGroup({
-		sliderControl2: new FormControl([18, 30])
-	});
-	options2: Options = {
+	minValue: number = 18;
+	maxValue: number = 30;
+	Options: Options = {
 		floor: 18,
 		ceil: 99,
-		step: 1,
-		showSelectionBar: true,
 		getPointerColor: (value: number): string => {
 			return '#34c6bb';
 		},
@@ -49,4 +48,38 @@ export class RegisterComponent {
 			return '#34c6bb';
 		}
 	};
+
+	// sliderControl: FormControl = new FormControl(50);
+	// options: Options = {
+	// 	floor: 0,
+	// 	ceil: 50,
+	// 	getPointerColor: (value: number): string => {
+	// 		return '#34c6bb';
+	// 	}
+	// };
+	// sliderForm: FormGroup = new FormGroup({
+	// 	sliderControl2: new FormControl([18, 30])
+	// });
+	// options2: Options = {
+	// 	floor: 18,
+	// 	ceil: 99,
+	// 	step: 1,
+	// 	showSelectionBar: true,
+	// 	getPointerColor: (value: number): string => {
+	// 		return '#34c6bb';
+	// 	},
+	// 	getSelectionBarColor: (value: number): string => {
+	// 		return '#34c6bb';
+	// 	}
+	// };
+
+	onFormSubmit(userForm: NgForm){
+		console.log(userForm);
+	}
+
+}
+
+interface attirance {
+	name: string;
+	selected: boolean;
 }
